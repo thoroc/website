082f784d22979815cd1c0a8e7debb6b6d89f99be
thoroc <thomas.a.roche@gmail.com>
2024-09-25 20:22:29 +0100

    enable eslint, prettier - disable mergify

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,11 +1,16 @@
-import { web } from 'projen';
-import { NodePackageManager } from 'projen/lib/javascript';
+import { web } from "projen";
+import { NodePackageManager } from "projen/lib/javascript";
 
 const project = new web.NextJsTypeScriptProject({
-  defaultReleaseBranch: 'main',
-  name: 'website',
+  defaultReleaseBranch: "main",
+  name: "website",
   projenrcTs: true,
   packageManager: NodePackageManager.PNPM,
+  jest: true,
+  eslint: true,
+  prettier: true,
+  mergify: false,
+  tailwind: true,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
