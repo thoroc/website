ed08e243e903879694e8c4b01c2d070ab52aedf9
thoroc <thomas.a.roche@gmail.com>
2024-09-28 21:35:55 +0100

    speed insight enabled

--- projenrc/nextjs/files/source/app/layout.ts
+++ projenrc/nextjs/files/source/app/layout.ts
@@ -8,6 +8,7 @@ export class Layout extends SourceCode {
     this.line(`import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import * as React from "react";
 import theme from "@/theme";
 
@@ -15,6 +16,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <SpeedInsights />
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
