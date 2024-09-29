d2966db44526fec4cae881f1c33133e25e55ee25
thoroc <thomas.a.roche@gmail.com>
2024-09-29 22:26:00 +0100

    fixing the pipeline - hopefully

--- projenrc/github/index.ts
+++ projenrc/github/index.ts
@@ -1,2 +1,30 @@
-export * from "./pipeline";
-export * from "./github";
+import { Component, github } from 'projen';
+import { TypeScriptProject } from 'projen/lib/typescript';
+import { CiPipeline } from './workflows';
+import { PullRequestTemplate } from './templates';
+
+export interface GithubOptions {
+  pnpmVersion: string;
+  templates?: { pullRequest?: boolean };
+}
+
+export class Github extends Component {
+  project: TypeScriptProject;
+
+  constructor(project: TypeScriptProject, options: GithubOptions) {
+    super(project);
+
+    const githubComponent = new github.GitHub(project, {
+      mergify: false,
+      pullRequestLint: false,
+    });
+
+    new CiPipeline(githubComponent, options.pnpmVersion);
+
+    if (options.templates?.pullRequest ?? true) {
+      new PullRequestTemplate(project);
+    }
+
+    this.project = project;
+  }
+}
