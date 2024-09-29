35d333f776e04db882bbf7db8d15aa93567c2453
thoroc <thomas.a.roche@gmail.com>
2024-09-29 22:55:32 +0100

    bare bone setup pnpm

--- projenrc/github/workflows/jobs/pnpm-action-setup.ts
+++ projenrc/github/workflows/jobs/pnpm-action-setup.ts
@@ -3,9 +3,9 @@ import { JobStep } from 'projen/lib/github/workflows-model';
 export const PnpmActionSetup: JobStep = {
   name: 'Setup pnpm',
   uses: 'pnpm/action-setup@v4',
-  with: {
-    // conflict with the version specified in the package.json
-    // version: pnpmVersion,
-    run_install: false,
-  },
+  // with: {
+  // conflict with the version specified in the package.json
+  // version: pnpmVersion,
+  // run_install: false,
+  // },
 };
