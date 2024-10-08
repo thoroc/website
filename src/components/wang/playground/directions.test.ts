837d9d68ff57d6c6263f91e17a84116e9e093951
thoroc <thomas.a.roche@gmail.com>
2024-10-08 21:16:05 +0100

    porting Directions

--- src/components/wang/playground/directions.test.ts
+++ src/components/wang/playground/directions.test.ts
@@ -1,77 +1,132 @@
-import { CardinalDirections, DiagonalDirections, shiftClockwise, shiftCounterClockwise } from './directions';
+import { Directions, Cardinal } from './directions';
 
-describe('CardinalDirections Enum', () => {
-  it('should have correct values', () => {
-    expect(CardinalDirections.NORTH).toBe(1);
-    expect(CardinalDirections.EAST).toBe(2);
-    expect(CardinalDirections.SOUTH).toBe(4);
-    expect(CardinalDirections.WEST).toBe(8);
+describe('Directions.Keys', () => {
+  it('should return an array of numbers representing the cardinal directions', () => {
+    const keys = Directions.Keys;
+    expect(keys).toEqual([1, 2, 4, 8]);
+  });
+
+  it('should have the correct length', () => {
+    const keys = Directions.Keys;
+    expect(keys.length).toBe(4);
+  });
+
+  it('should contain only numbers', () => {
+    const keys = Directions.Keys;
+    keys.forEach((key) => {
+      expect(typeof key).toBe('number');
+    });
   });
 });
 
-describe('DiagonalDirections Enum', () => {
-  it('should have correct values', () => {
-    expect(DiagonalDirections.NORTH_EAST).toBe(3);
-    expect(DiagonalDirections.SOUTH_EAST).toBe(6);
-    expect(DiagonalDirections.SOUTH_WEST).toBe(12);
-    expect(DiagonalDirections.NORTH_WEST).toBe(9);
+describe('Directions.CardinalList', () => {
+  it('should return an array of cardinal directions', () => {
+    const cardinalList = Directions.CardinalList;
+    expect(cardinalList).toEqual([Cardinal.North, Cardinal.East, Cardinal.South, Cardinal.West]);
   });
 });
 
-describe('shiftClockwise', () => {
-  it('should shift direction clockwise correctly', () => {
-    const shift1 = shiftClockwise({ direction: CardinalDirections.NORTH, shift: 1 });
-    expect(shift1).toBe(CardinalDirections.EAST);
-
-    const shift2 = shiftClockwise({ direction: shift1, shift: 1 });
-    expect(shift2).toBe(CardinalDirections.SOUTH);
-
-    const shift3 = shiftClockwise({ direction: shift2, shift: 1 });
-    expect(shift3).toBe(CardinalDirections.WEST);
-
-    const shift4 = shiftClockwise({ direction: shift3, shift: 1 });
-    expect(shift4).toBe(CardinalDirections.NORTH);
-  });
-
-  it('should handle multiple shifts correctly', () => {
-    const initial = CardinalDirections.NORTH;
-
-    const shift1 = shiftClockwise({ direction: initial, shift: 2 });
-    expect(shift1).toBe(CardinalDirections.SOUTH);
-
-    const shift2 = shiftClockwise({ direction: initial, shift: 3 });
-    expect(shift2).toBe(CardinalDirections.WEST);
-
-    const shift3 = shiftClockwise({ direction: initial, shift: 4 });
-    expect(shift3).toBe(CardinalDirections.NORTH);
+describe('Directions.ReversedCardinalList', () => {
+  it('should return an array of cardinal directions in reversed order', () => {
+    const reversedCardinalList = Directions.ReversedCardinalList;
+    expect(reversedCardinalList).toEqual([Cardinal.West, Cardinal.South, Cardinal.East, Cardinal.North]);
   });
 });
 
-describe('shiftCounterClockwise', () => {
-  it('should shift direction counter-clockwise correctly', () => {
-    const shift1 = shiftCounterClockwise({ direction: CardinalDirections.NORTH, shift: 1 });
-    expect(shift1).toBe(CardinalDirections.WEST);
+describe('Directions.MinValue', () => {
+  it('should return the minimum value of the cardinal directions', () => {
+    const minValue = Directions.MinValue;
+    expect(minValue).toBe(1);
+  });
+});
 
-    const shift2 = shiftCounterClockwise({ direction: shift1, shift: 1 });
-    expect(shift2).toBe(CardinalDirections.SOUTH);
+describe('Directions.MaxValue', () => {
+  it('should return the maximum value of the cardinal directions', () => {
+    const maxValue = Directions.MaxValue;
+    expect(maxValue).toBe(8);
+  });
+});
 
-    const shift3 = shiftCounterClockwise({ direction: shift2, shift: 1 });
-    expect(shift3).toBe(CardinalDirections.EAST);
+describe('Directions.All', () => {
+  it('should return an array of all cardinal directions', () => {
+    const all = Directions.All;
+    expect(all).toEqual([Cardinal.North, Cardinal.East, Cardinal.South, Cardinal.West]);
+  });
+});
 
-    const shift4 = shiftCounterClockwise({ direction: shift3, shift: 1 });
-    expect(shift4).toBe(CardinalDirections.NORTH);
+describe('Directions.OppositeDirection', () => {
+  it('should return the opposite direction', () => {
+    expect(Directions.OppositeDirection(Cardinal.North)).toBe(Cardinal.South);
+    expect(Directions.OppositeDirection(Cardinal.East)).toBe(Cardinal.West);
+    expect(Directions.OppositeDirection(Cardinal.South)).toBe(Cardinal.North);
+    expect(Directions.OppositeDirection(Cardinal.West)).toBe(Cardinal.East);
+  });
+});
+
+describe('Directions.Next', () => {
+  it('should return the next direction in sequence', () => {
+    expect(Directions.Next(Cardinal.North)).toBe(Cardinal.East);
+    expect(Directions.Next(Cardinal.East)).toBe(Cardinal.South);
+    expect(Directions.Next(Cardinal.South)).toBe(Cardinal.West);
+    expect(Directions.Next(Cardinal.West)).toBe(Cardinal.North);
+  });
+});
+
+describe('Directions.Previous', () => {
+  it('should return the previous direction in sequence', () => {
+    expect(Directions.Previous(Cardinal.North)).toBe(Cardinal.West);
+    expect(Directions.Previous(Cardinal.East)).toBe(Cardinal.North);
+    expect(Directions.Previous(Cardinal.South)).toBe(Cardinal.East);
+    expect(Directions.Previous(Cardinal.West)).toBe(Cardinal.South);
+  });
+});
+
+describe('Directions.GetWeight', () => {
+  it('should return the sum of the directions', () => {
+    const weight = Directions.GetWeight([Cardinal.North, Cardinal.East]);
+    expect(weight).toBe(3);
+  });
+});
+
+describe('Directions.GetDirections', () => {
+  it('should return the directions corresponding to the weight', () => {
+    expect(Directions.GetDirections(1)).toEqual([Cardinal.North]);
+    expect(Directions.GetDirections(2)).toEqual([Cardinal.East]);
+    expect(Directions.GetDirections(3)).toEqual([Cardinal.East, Cardinal.North]);
+    expect(Directions.GetDirections(4)).toEqual([Cardinal.South]);
+    expect(Directions.GetDirections(5)).toEqual([Cardinal.South, Cardinal.North]);
+    expect(Directions.GetDirections(6)).toEqual([Cardinal.South, Cardinal.East]);
+  });
+});
+
+describe('Directions.GetExcludedDirections', () => {
+  it('should return the directions not included in the weight', () => {
+    const excludedDirections = Directions.GetExcludedDirections(3);
+    expect(excludedDirections).toEqual([Cardinal.South, Cardinal.West]);
+  });
+});
+
+describe('Directions.GetRandomDirection', () => {
+  it('should return a random direction not included in the weight mask', () => {
+    const randomDirection = Directions.GetRandomDirection(3);
+    expect([Cardinal.South, Cardinal.West]).toContain(randomDirection);
   });
 
-  it('should handle multiple shifts correctly', () => {
-    const initial = CardinalDirections.NORTH;
+  it('should return null if all directions are included in the weight mask', () => {
+    const randomDirection = Directions.GetRandomDirection(15);
+    expect(randomDirection).toBeNull();
+  });
+});
 
-    const shift1 = shiftCounterClockwise({ direction: initial, shift: 2 });
-    expect(shift1).toBe(CardinalDirections.SOUTH);
+describe('Directions.GetDirection', () => {
+  it('should return the direction corresponding to the value', () => {
+    expect(Directions.GetDirection(1)).toBe(Cardinal.North);
+    expect(Directions.GetDirection(2)).toBe(Cardinal.East);
+    expect(Directions.GetDirection(4)).toBe(Cardinal.South);
+    expect(Directions.GetDirection(8)).toBe(Cardinal.West);
+  });
 
-    const shift2 = shiftCounterClockwise({ direction: initial, shift: 3 });
-    expect(shift2).toBe(CardinalDirections.EAST);
-
-    const shift3 = shiftCounterClockwise({ direction: initial, shift: 4 });
-    expect(shift3).toBe(CardinalDirections.NORTH);
+  it('should throw an error if the value is not a valid direction', () => {
+    expect(() => Directions.GetDirection(16)).toThrow(RangeError);
   });
 });
--- src/components/wang/playground/directions.ts
+++ src/components/wang/playground/directions.ts
@@ -1,42 +1,103 @@
-export enum CardinalDirections {
-  NORTH = 1,
-  EAST = 2,
-  SOUTH = 4,
-  WEST = 8,
+export enum Cardinal {
+  North = 1,
+  East = 2,
+  South = 4,
+  West = 8,
+  //    NorthEast = 16,
+  //    SouthEast = 32,
+  //    SouthWest = 64,
+  //    NorthWest = 128,
 }
 
-export enum DiagonalDirections {
-  NORTH_EAST = 3,
-  SOUTH_EAST = 6,
-  SOUTH_WEST = 12,
-  NORTH_WEST = 9,
+export class Directions {
+  private static HalfCount = Math.floor(Directions.getCardinalList().length / 2);
+
+  private static RandomInt(max: number): number {
+    return Math.floor(Math.random() * max);
+  }
+
+  public static get Keys(): number[] {
+    return Object.values(Cardinal).filter((value) => typeof value === 'number') as number[];
+  }
+
+  public static get CardinalList(): Cardinal[] {
+    return Directions.getCardinalList();
+  }
+
+  public static get ReversedCardinalList(): Cardinal[] {
+    return Directions.getCardinalList().sort((a, b) => b - a);
+  }
+
+  public static get MinValue(): number {
+    return Math.min(...Directions.Keys);
+  }
+
+  public static get MaxValue(): number {
+    return Math.max(...Directions.Keys);
+  }
+
+  public static get All(): Cardinal[] {
+    return [
+      Cardinal.North,
+      //            Cardinal.NorthEast,
+      Cardinal.East,
+      //            Cardinal.SouthEast,
+      Cardinal.South,
+      //            Cardinal.SouthWest,
+      Cardinal.West,
+      //            Cardinal.NorthWest,
+    ];
+  }
+
+  public static OppositeDirection(direction: Cardinal): Cardinal {
+    let value: number =
+      direction > Directions.CardinalList[Directions.HalfCount - 1]
+        ? direction >> Directions.HalfCount
+        : direction << Directions.HalfCount;
+
+    return Directions.GetDirection(value);
+  }
+
+  public static Next(direction: Cardinal): Cardinal {
+    let value: number = direction === Directions.MaxValue ? Directions.MinValue : direction << 1;
+
+    return Directions.GetDirection(value);
+  }
+
+  public static Previous(direction: Cardinal): Cardinal {
+    let value: number = direction === Directions.MinValue ? Directions.MaxValue : direction >> 1;
+
+    return Directions.GetDirection(value);
+  }
+
+  public static GetWeight(directions: Cardinal[]): number {
+    return directions.reduce((sum, direction) => sum + direction, 0);
+  }
+
+  public static GetDirections(weight: number): Cardinal[] {
+    let cardinal = weight as Cardinal;
+    return Directions.ReversedCardinalList.filter((x) => (cardinal & x) !== 0);
+  }
+
+  public static GetExcludedDirections(weight: number): Cardinal[] {
+    return Directions.All.filter((x) => !Directions.GetDirections(weight).includes(x));
+  }
+
+  public static GetRandomDirection(weightMask: number = 0): Cardinal | null {
+    let availableCardinals = Directions.GetExcludedDirections(weightMask);
+    if (availableCardinals.length === 0) return null;
+
+    return availableCardinals[Directions.RandomInt(availableCardinals.length)];
+  }
+
+  public static GetDirection(value: number): Cardinal {
+    if (Object.values(Cardinal).includes(value)) {
+      return value as Cardinal;
+    }
+    throw new RangeError(`${value} is not a valid value for a direction. Maximum value is ${Directions.MaxValue}.`);
+  }
+
+  private static getCardinalList(): Cardinal[] {
+    return Object.values(Cardinal).filter((value) => typeof value === 'number') as Cardinal[];
+  }
 }
-
-interface DirectionShift {
-  direction: number;
-  shift: number;
-}
-
-/**
- * Shifts a given direction value clockwise by a specified number of positions.
- *
- * @param direction - The initial direction value to be shifted.
- * @param shift - The number of positions to shift the direction value.
- * @returns The new direction value after being shifted clockwise.
- */
-export const shiftClockwise = (props: DirectionShift): number => {
-  const { direction, shift } = props;
-  return (direction << shift) | (direction >>> (4 - shift));
-};
-
-/**
- * Shifts a 4-bit direction value counterclockwise by a specified number of positions.
- *
- * @param direction - The 4-bit direction value to be shifted.
- * @param shift - The number of positions to shift the direction value.
- * @returns The new direction value after the shift.
- */
-export const shiftCounterClockwise = (props: DirectionShift): number => {
-  const { direction, shift } = props;
-  return (direction >>> shift) | (direction << (4 - shift));
-};
